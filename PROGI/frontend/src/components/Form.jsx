bdc393b5b3905d6cd734dad81d3fa54ee70062c7
tomislav-marenic <tomislav.marenic@fer.hr>
2024-11-16 23:07:22 +0100

    Commit koda koji smo prezentirali 14.11.

---
+++ PROGI/frontend/src/components/Form.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate, Link } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import '../styles/Form.css'
+import LoadingIndicator from "./LoadingIndicator";
+
+function Form({ route, method }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const name = method === "login" ? "Login" : "Register";
+  let customDiv;
+  // let dropdown;
+
+  if (name === "Login") {
+    customDiv = <div>Don't have an account yet? <Link to='/register'>Register</Link></div>
+  } 
+  if (name === "Register") {
+    customDiv = <div>Already have an account? <Link to="/login">Login</Link></div>
+
+  }
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, {username, password});
+
+      if (method === 'login') {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate('/home')
+      } else {
+        navigate('/login');
+      }
+
+    } catch (error) {
+
+      alert(error);
+
+    } finally {
+
+      setLoading(false);
+
+    }
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>{name}</h1>
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      {/* {dropdown} */}
+      {/* <input>{dropdown}</input> */}
+      {loading && <LoadingIndicator/>}
+      <button className="form-button" type="submit">
+        {name}
+      </button>
+      <div>{customDiv}</div>
+    </form>
+  );
+}
+
+export default Form
