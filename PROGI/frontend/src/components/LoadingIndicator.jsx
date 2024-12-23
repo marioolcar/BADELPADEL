bc31aa7c877cad26455e03a4a7e7f90e66d6e044
Mario Olčar <47773816+marioolcar@users.noreply.github.com>
2024-12-23 18:57:56 +0100

    Ispravljanje strukture foldera

--- PROGI/frontend/src/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/LoadingIndicator.css'
-
-const LoadingIndicator = () => {
-    return <div>
-        <div className='loader'></div>
-    </div>
-}
-
-export default LoadingIndicator
