93a6e9437da6b93b5adb8f40f0add015c7a3d415
Josip <josip.cukrov@fer.unizg.hr>
2025-01-16 10:09:33 +0100

    Front fix-evi i izbacivanje par stvari sa back-a
    Maknuo api/user/ path i cijeli user_view, maknuo broj like-ova i komentara za post i komentar. Na frontu hrpa manjih fix-eva i dodana prva verzija google Oauth 2.0 koja zasad dekodira i salje na backend da user-a stvori

--- src/frontend/src/components/Header.jsx
+++ src/frontend/src/components/Header.jsx
@@ -9,10 +9,9 @@ function Header(){
             <img src={logo} alt="logo" height={75}></img>
             <nav className="navigation">
                 <a className="header-links" href="/">Home</a>
-                <a className="header-links" href="/profile">Profile</a>
-                <a className="header-links" href="/fields">Fields</a>
-                <a className="header-links" href="/tournaments">Tournaments</a>
-                <a className="header-links" href="/owners">Owners</a>
+                <a className="header-links" href="/fields">Tereni</a>
+                <a className="header-links" href="/tournaments">Turniri</a>
+                <a className="header-links" href="/owners">Vlasnici</a>
                 <a className="header-links" href="/notes">Notes</a>
                 {!localStorage.getItem(ACCESS_TOKEN) ? 
                 <a className="header-links" href="/login" id="sign-in-button">Sign in</a> :
